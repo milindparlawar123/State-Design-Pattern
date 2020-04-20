069482feccd00281e4d62ef1d24d8d0ee823b501
milindparlawar123 <50922528+milindparlawar123@users.noreply.github.com>
2020-04-19 22:45:40 -0400

    states base code added

---
+++ personSpending/src/personSpending/states/HasLuxuriousState.java
@@ -0,0 +1,23 @@
+package personSpending.states;
+
+public class HasLuxuriousState implements PersonStates{
+
+	@Override
+	public void basic(Integer incoming) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void luxurious(Integer incoming) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extravagant(Integer incoming) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ personSpending/src/personSpending/states/PersonStates.java
@@ -0,0 +1,8 @@
+package personSpending.states;
+
+public interface PersonStates {
+
+	void basic(Integer incoming);
+	void luxurious (Integer incoming);
+	void extravagant(Integer incoming);
+}
